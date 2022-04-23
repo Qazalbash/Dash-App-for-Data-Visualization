9a76a4e6d67dcb388f47e4701719c17be3d74122
Meesum A. Qazalbash <62182585+MeesumAliQazalbash@users.noreply.github.com>
2022-04-24 01:24:26 +0500

    skew heap modified

--- src/skew_heap.py
+++ src/skew_heap.py
@@ -1,4 +1,53 @@
-from skew_node import *
+from typing import Any, Optional
+
+
+class Node:
+    """A single Node in a binary tree."""
+
+    def __init__(self, value, left=None, right=None) -> None:
+        self._value: Any = value
+        self._left: Node = left
+        self._right: Node = right
+
+    def __repr__(self) -> str:
+
+        reprstr = f"Node(value: {self._value}, "
+
+        if self._left == None:
+            reprstr += f"left: {None}, "
+        else:
+            reprstr += f"left: {self._left._value}, "
+
+        if self._right == None:
+            reprstr += f"right: {None})"
+        else:
+            reprstr += f"right: {self._right._value})"
+
+        return reprstr
+
+    @property
+    def value(self) -> Any:
+        return self._value
+
+    @property
+    def left(self) -> "Node":
+        return self._left
+
+    @property
+    def right(self) -> "Node":
+        return self._right
+
+    @value.setter
+    def value(self, va: Any) -> None:
+        self._value = va
+
+    @left.setter
+    def left(self, l: Any) -> None:
+        self._left = l
+
+    @right.setter
+    def right(self, ri: Any) -> None:
+        self._right = ri
 
 
 class SkewHeap:
@@ -21,7 +70,7 @@ class SkewHeap:
     def push(self, value: Any):
         """Add an item to this heap."""
         node = Node(value, None, None)
-        self.root = merge(self.root, node)
+        self.root = self.merge(self.root, node)
 
     def pop(self):
         """Remove the least item in this heap and return it."""
@@ -29,7 +78,7 @@ class SkewHeap:
             raise ValueError("Cannot pop empty SkewHeap")
         else:
             value = self.root.value
-            self.root = merge(self.root.left, self.root.right)
+            self.root = self.merge(self.root.left, self.root.right)
             return value
 
     def peek(self):
@@ -42,7 +91,7 @@ class SkewHeap:
     def union(self, other: "SkewHeap") -> "SkewHeap":
         """Return a new heap which contains all the items of this and another heap combined."""
         ret = SkewHeap()
-        ret.root = merge(self.root, other.root)
+        ret.root = self.merge(self.root, other.root)
         return ret
 
     def __add__(self, other: "SkewHeap") -> "SkewHeap":
@@ -51,7 +100,7 @@ class SkewHeap:
 
     def __len__(self) -> int:
         """Return the number of items in this heap."""
-        return count(self.root)
+        return self.count(self.root)
 
     def __bool__(self) -> bool:
         """Return true iff the heap is non-empty."""
@@ -59,4 +108,48 @@ class SkewHeap:
 
     def depth(self) -> int:
         """Return the depth of the tree used to represent this skew heap."""
-        return depth(self.root)
+        if self.root is None:
+            return 0
+        return 1 + max(self.depth(self.root.left), self.depth(self.root.right))
+
+    def dfs(self, visited: Optional[Node], node: Node) -> Optional[Node]:
+        if node not in visited:
+            visited.append(node)
+            neigh = []
+            if node.left != None:
+                neigh.append(node.left)
+            if node.right != None:
+                neigh.append(node.right)
+            for neighbor in neigh:
+                self.dfs(visited, neighbor)
+        return visited
+
+    @property
+    def all_nodes(self) -> Optional[Any]:
+        return [i.value for i in self.dfs([], self.root)]
+
+    @staticmethod
+    def merge(p: Optional[Node], q: Optional[Node]) -> Node:
+        """
+        Implements the critical "merge" operation which is
+        used by all operations on a SkewHeap. The merge operation
+        does not mutate either tree but returns a new tree which
+        contains the least item at the root and is in heap order.
+        The resulting tree is not necessarily balanced.
+        """
+        if p is None:
+            return q
+        if q is None:
+            return p
+
+        if q.value < p.value:
+            p, q = q, p
+
+        return Node(p.value, SkewHeap.merge(p.right, q), p.left)
+
+    @staticmethod
+    def count(node: Optional[Node]) -> int:
+        """Count the total number of nodes in a tree rooted at this node."""
+        if node is None:
+            return 0
+        return 1 + SkewHeap.count(node.left) + SkewHeap.count(node.right)
