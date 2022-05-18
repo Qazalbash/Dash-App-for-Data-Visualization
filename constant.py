3ac29d0027b40c42591ed07a94b0243e29e94ba6
Meesum A. Qazalbash <mesumali26.ma@gmail.com>
2022-05-18 10:43:29 +0500

    map_style changed

--- constant.py
+++ constant.py
@@ -16,7 +16,8 @@ mapbox_access_token = "pk.eyJ1IjoicGxvdGx5bWFwYm94IiwiYSI6ImNrOWJqb2F4djBnMjEzbG
 mapbox_style = "mapbox://styles/plotlymapbox/cjvprkf3t1kns1cqjxuxmwixz"
 
 DEFAULT_CENTER = dict(lat=38.72490, lon=-95.61446)
-DEFAULT_MAPBOX_STYLE = "carto-positron"
+# DEFAULT_MAPBOX_STYLE = "carto-positron"
+DEFAULT_MAPBOX_STYLE = "open-street-map"
 
 YEARS = [2020, 2021, 2022]
 MONTHS = {
